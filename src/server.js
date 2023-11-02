b3fb52878d7440910f92050bb0b6e1e5f288a28e
franco <rojasfranco204@gmail.com>
2023-11-02 15:13:32 -0300

    Agregi JWT

--- src/server.js
+++ src/server.js
@@ -1,16 +1,23 @@
 
 require('dotenv').config();
-const express = require('express')
-const server = express()
-const usuarioRouter = require('./routes/usuarioRutes')
+const express = require('express');
+const server = express();
+const bodyParser = require('body-parser');
+
+const usuarioRouter = require('./routes/usuarioRutes');
+const autenticacionRouter = require('./routes/autenticacionRoutes');
 const port = 3000
+
+
 
 server.get('/', (req, res) => {
   res.send('hola mundo')
-})
-
+});
+//middleware
+server.use(bodyParser.json());
 server.use(usuarioRouter);
+server.use(autenticacionRouter);
 
 server.listen(port, () => {
   console.log(`Servidor corriendo en el puerto  ${port}`)
-})
+});
